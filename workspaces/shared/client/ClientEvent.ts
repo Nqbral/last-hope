36ff1c876f23488173f644916ab37af8ec82def9
Simon Chevalier <sim.chevalier@gmail.com>
2025-04-30 12:34:06 +0200

    SHARED - Add client event + package.json

---
+++ workspaces/shared/client/ClientEvent.ts
@@ -0,0 +1,15 @@
+export const CLIENT_EVENTS = {
+  // Lobby
+  LOBBY_CREATE: "client.lobby.create",
+  LOBBY_JOIN: "client.lobby.join",
+  LOBBY_LEAVE: "client.lobby.leave",
+  LOBBY_DELETE: "client.lobby.delete",
+} as const;
+
+export type ClientSocketEvents = {
+  // Lobby
+  [CLIENT_EVENTS.LOBBY_CREATE]: undefined;
+  [CLIENT_EVENTS.LOBBY_JOIN]: undefined;
+  [CLIENT_EVENTS.LOBBY_LEAVE]: undefined;
+  [CLIENT_EVENTS.LOBBY_DELETE]: undefined;
+};
