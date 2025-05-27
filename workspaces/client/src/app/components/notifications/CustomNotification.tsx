2cdac946d9bbd0d62b5f82a3de0eb37346b99a19
Simon Chevalier <sim.chevalier@gmail.com>
2025-05-27 20:42:42 +0200

    CLIENT - Responsive Custom Notifications

--- workspaces/client/src/app/components/notifications/CustomNotification.tsx
+++ workspaces/client/src/app/components/notifications/CustomNotification.tsx
@@ -16,8 +16,10 @@ export default function CustomNotification({ data }: CustomNotificationProps) {
   return (
     <div className={`${orbitron.className}`}>
       <div className="flex w-full flex-col items-center gap-4">
-        <h3 className="text-primary text-lg">{data.title}</h3>
-        <p className="text-center text-sm text-white">{data.content}</p>
+        <h3 className="text-primary text-base sm:text-lg">{data.title}</h3>
+        <p className="text-center text-sm text-white sm:text-base">
+          {data.content}
+        </p>
       </div>
     </div>
   );
