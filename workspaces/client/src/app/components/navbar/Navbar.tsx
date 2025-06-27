ea4bfdcd2556909f62d3b7e2d94f49e01f17fa3b
Simon Chevalier <sim.chevalier@gmail.com>
2025-06-27 15:58:17 +0200

    CLIENT - correct alt logo

--- workspaces/client/src/app/components/navbar/Navbar.tsx
+++ workspaces/client/src/app/components/navbar/Navbar.tsx
@@ -34,7 +34,7 @@ export default function Navbar() {
           <Image
             src={LastHopeLogo}
             className="w-24 sm:w-28 md:w-32"
-            alt="nqbral-games-logo-navbar"
+            alt="last-hope-logo-navbar"
           />
         </div>
       </Link>
