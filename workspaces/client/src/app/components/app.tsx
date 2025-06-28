8ecf4d8034c9d0bd4e6e6a97efbfaa180f519950
Simon Chevalier <sim.chevalier@gmail.com>
2025-06-29 01:21:30 +0200

    CLIENT - move script json-ld correctly + meta tags + plain axios refresh request

--- workspaces/client/src/app/components/app.tsx
+++ workspaces/client/src/app/components/app.tsx
@@ -16,6 +16,25 @@ export default function App({
     <AuthProvider>
       <SocketProvider>
         <html className={`${orbitron.className}`}>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#1A1A1A" />
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                '@context': 'https://schema.org',
+                '@type': 'WebSite',
+                name: 'Last Hope',
+                url: 'https://last-hope.nqbral-games.fr/',
+                alternateName: 'Nqbral Games',
+                inLanguage: 'fr',
+                sameAs: [
+                  'https://nqbral-games.fr/',
+                  'https://shadow-network.nqbral-games.fr/',
+                ],
+              }),
+            }}
+          />
           <body>{children}</body>
         </html>
       </SocketProvider>
--- workspaces/client/src/app/layout.tsx
+++ workspaces/client/src/app/layout.tsx
@@ -13,27 +13,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <script
-        id="json-ld"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'WebSite',
-            name: 'Last Hope',
-            url: 'https://last-hope.nqbral-games.fr/',
-            alternateName: 'Nqbral Games',
-            inLanguage: 'fr',
-            sameAs: [
-              'https://nqbral-games.fr/',
-              'https://shadow-network.nqbral-games.fr/',
-            ],
-          }),
-        }}
-      />
-      <App>{children}</App>
-    </>
-  );
+  return <App>{children}</App>;
 }
